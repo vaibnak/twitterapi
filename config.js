79e9d0790ec16a9d538c8a5887e4d3ca4319705e
vaibnak7 <balamanju780@gmail.com>
2018-10-12 09:27:39 +0530

    all functionalities aded

---
+++ config.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+
+  consumer_key: 'SIPSRlJrVsZPUChEZrm9SoIQc' ,
+  consumer_secret:'QdOFikYwkPHx4htJybRZXWrLxYOvlTIhqyyDoykpEoagkvsvf6' ,
+  access_token_key: '1050451718172024837-S6QNX0sP6LTqJVYOOhjGbWVmdu4gIH' ,
+  access_token_secret: '3BuTu9lTo1N4DLSTRDhWaU3NRJl1fllphb74kMhuiaj58'
+
+}
